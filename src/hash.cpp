816f21b48fc7a47336b32c0b932d8d92a7db56aa
anryisreal <Krutt228zz@mail.ru>
2024-08-20 11:31:17 +0300

    Implementation of all interactions with HashTable. Passing a custom function for hashing UserHashFunc. (The standard function only works for int type)

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,68 +1,143 @@
+#ifndef _HASH_TABLE_H_
+#define _HASH_TABLE_H_
+ 
 #include <vector>
 #include <initializer_list>
 #include <utility>
 #include <iostream>
+#include <set>
 
 namespace std _GLIBCXX_VISIBILITY(default) {
-    template <typename Type1, typename Type2>
+    template <typename Type1, typename Type2, int(*UserHashFunc)(Type1 key)=nullptr>
     class HashTable {
     private:
         class Info {
             public:
-                Type2 operator=(const Type1 _key) {
+                Info() : ht{nullptr} { }
+                Info(HashTable* ht, Type1 _key) : ht{ht}, i_key{_key} { }
+                Info(HashTable* ht, std::pair<const Type1, Type2> pair) : ht{ht}, i_key{pair.first}, i_value{pair.second}, isValue{true} { }
+                Type2 operator=(const Type2 value) {
+                    i_value = value;
+                    if (!isValue) {
+                        ht->__insert(std::pair(i_key, i_value));
+                    }
                     return i_value;
                 }
                 operator Type2() const {
-                    
+                    if (!isValue) {
+                        throw ("IndexError"); 
+                    }
+                    return i_value;
+                }
+                Type1 first() {
+                    return i_key;
+                }
+                Type2 second() {
+                    return i_value;
+                }
+                bool isfull() {
+                    return isValue;
                 }
             private:
-                const Type1 i_key;
+                HashTable* ht {nullptr};
+                Type1 i_key;
                 Type2 i_value;
+                bool isValue{false};
         };
         int h_capacity {0}; // Real size of the HashTable 
         int count {0}; // The number of items(Info) in the HashTable
-        std::vector<Info> ht; // Array of pointers to on items(Info)  ---Our HashTable---
+        Info* ht{nullptr}; // Array of pointers to on items(Info)  ---Our HashTable---
+        std::set<int> keys;
+        const int _MAX_SIZE_ = 3000000; 
+
+
     public:
         HashTable() = default;
         HashTable(std::initializer_list<std::pair<const Type1, Type2>>&& ht) {
             this->count = ht.size();
-            for (auto x: ht) {
-                this->ht.push_back(x);
+            this->ht = new Info[count];
+            h_capacity = count;
+            for (auto pair: ht) {
+                __insert(pair);
             }
-            this->h_capacity = this->ht.capacity();
+        }
+        ~HashTable() {
+            delete[] ht;
         }
 
-        operator[] (Type1 _key) {
-            for (auto pair: ht) {
-                if (pair.first == _key) {
-
+        Info operator[] (Type1 _key) {
+            for (auto& pair: ht) {
+                if (pair.first() == _key) {
+                    return pair;
                 }
             }
+            return Info(this, _key);
         }
+
         int size() {
             return count;
         }
+
         int capacity () {
             return h_capacity;
         }
-        void insert() {
-
-        }
+        
         void print() {
-            for (auto item: ht) {
-                std::cout << item.first << " : " << item.second << std::endl;
+            for (auto index: keys) {
+                cout << ht[index] << endl;
+            }
+        }
+
+        void __resize(int index) {
+            int size = h_capacity;
+            if (index > h_capacity) {
+                if (h_capacity * 2 > _MAX_SIZE_)  {
+                    h_capacity = _MAX_SIZE_;
+                }
+                else {
+                    h_capacity *= 2;
+                }
+                Info* newht = new Info[h_capacity];
+                copy(ht, ht + size, newht);
+                delete[] ht;
+                ht = newht;
+            }
+        }
+
+        void __insert(std::pair<const Type1, Type2> pair) {
+            int index = 0;
+            if (!UserHashFunc) {
+                index = HashFunction(pair.first);
+            }
+            else {
+                index = UserHashFunc(pair.first);
+            }
+            cout << index << " " << h_capacity << endl;
+            __resize(index);
+            HashCollision(index, pair.first);
+            this->ht[index] = Info(this, pair);
+            keys.insert(index);
+        }
+
+        int HashFunction(Type1 key) {
+            // --- CONST ---
+            float a = 143.67;
+            float b = 58.733;
+            int p = 13;
+            int m = 5;
+            // --- CONST ---
+            int index = 0;
+            index = ((int)(a * key + b) % p) % m;
+
+            return index;
+        }
+        void HashCollision(int& index, Type1 key) {
+            while (ht[index].isfull() && ht[index].first() != key) {
+                index += 10;
+                __resize(index);
             }
         }
 
     };
-}
-
-
-int main() {
-    typedef std::HashTable <int, int> _HT;
-    _HT a {{1, 2}, {3, 4}, {5, 6}};
-    _HT b = a;
-    b.print();
-    std::cout << b.capacity();
-    return 0;
-}
+}   
+#endif
